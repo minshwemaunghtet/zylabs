c00756012fb03abc3466078e0fec801645dbc051
minshwemaunghtet <minshwemaunghtet@gmail.com>
2023-02-24 21:33:07 -0600

    6.15

---
+++ section6/LAB_ JavaScript Arrays/index.js
@@ -0,0 +1,35 @@
+function calcWordFrequencies() {
+    var words = prompt().split(" ");     
+    var unique_words = [], freqencies = [], count = 0;      
+    for (var i = 0; i < words.length; i++) {         
+        var found = false;         
+        
+        for (var j = 0; j < count; j++) {             
+            if (words[i] === unique_words[j]) {                 
+                freqencies[j] = freqencies[j] + 1;                 
+                found = true;             
+            }         
+        }          
+        
+        if (!found) {             
+            unique_words[count] = words[i];             
+            freqencies[count] = 1;             
+            count++;         
+        }    
+    }     
+    
+    for (var i = 0; i < words.length; i++) {         
+        var result = 0;         
+        for (var j = 0; j < count; j++) {             
+            if (words[i] === unique_words[j]) {                 
+                result = freqencies[j];                 
+                break;             
+            }         
+        }         
+        console.log(words[i] + " " + result);
+        
+    }
+}
+  
+// Function call
+// calcWordFrequencies();
