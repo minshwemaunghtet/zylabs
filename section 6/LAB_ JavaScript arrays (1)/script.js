69509f93d05ea326a00eb9bdb83e8cf2451da89b
minshwemaunghtet <minshwemaunghtet@gmail.com>
2023-02-24 22:08:13 -0600

    arrays

---
+++ section 6/LAB_ JavaScript arrays (1)/script.js
@@ -0,0 +1,37 @@
+function divideArray(numbers) {
+    let evenNums = [];
+    let oddNums = [];
+  
+    // Divide the array into even and odd numbers
+    for (let i = 0; i < numbers.length; i++) {
+      if (numbers[i] % 2 === 0) {
+        evenNums.push(numbers[i]);
+      } else {
+        oddNums.push(numbers[i]);
+      }
+    }
+  
+    // Sort the arrays
+    evenNums.sort(function(a, b) { return a - b });
+    oddNums.sort(function(a, b) { return a - b });
+  
+    // Output the arrays to the console
+    console.log("Even numbers:");
+    if (evenNums.length === 0) {
+      console.log("None");
+    } else {
+      for (let i = 0; i < evenNums.length; i++) {
+        console.log(evenNums[i]);
+      }
+    }
+  
+    console.log("Odd numbers:");
+    if (oddNums.length === 0) {
+      console.log("None");
+    } else {
+      for (let i = 0; i < oddNums.length; i++) {
+        console.log(oddNums[i]);
+      }
+    }
+  }
+  
