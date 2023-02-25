9bca515ff58c2f64ddfcacf072f0e538e36577d5
minshwemaunghtet <minshwemaunghtet@gmail.com>
2023-02-25 16:53:40 -0600

    snowman

--- section 8/LAB_ Snowman canvas/snowman.js
+++ section 8/LAB_ Snowman canvas/snowman.js
@@ -33,13 +33,48 @@ function drawSnowflakes(canvas) {
 // Complete the functions below
 
 function drawSnowText(canvas) {
+   // get the context
+   var context = canvas.getContext("2d");
 
+   // set font styles
+   context.font = "80px Verdana";
+   context.textAlign = "center";
+   context.textBaseline = "top";
+   context.fillStyle = "blue";
+   context.fillText("SNOW",canvas.width/2,10);
 }
 
 function drawSnowman(canvas) { 
+   // get the context
+   var context = canvas.getContext("2d");
 
+   // setup snowman styles
+   //  context.fillStyle = "white";
+   context.beginPath();
+   context.arc(150, 200, 50, 0, 2 * Math.PI);
+   context.fillStyle = "white";
+   context.fill();
+
+   context.beginPath();
+   context.arc(150, 120, 40, 0, 2 * Math.PI);
+   context.fillStyle = "white";
+   context.fill();
+
+   context.beginPath();
+   context.arc(150, 60, 25, 0, 2 * Math.PI);
+   context.fillStyle = "white";
+   context.fill();
 }
 
 function drawSingleFlake(canvas, x, y) {
+   // get the context
+   var context = canvas.getContext("2d");
 
+   // setup snowflake properties and style
+   context.moveTo(x,y);
+   context.lineTo(x+flakeSize/2,y+flakeSize/2);
+   context.lineTo(x,y+flakeSize);
+   context.lineTo(x-flakeSize/2,y+flakeSize/2);
+   context.fillStyle = "white";
+   context.fill();
 }
