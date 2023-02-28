c690322df79d4c64d009c2df7f6b85c3f9aff708
minshwemaunghtet <minshwemaunghtet@gmail.com>
2023-02-27 19:39:02 -0600

    temp converter

--- section 7/LAB_ Temperature converter/convert.js
+++ section 7/LAB_ Temperature converter/convert.js
@@ -1,13 +1,76 @@
 window.addEventListener("DOMContentLoaded", domLoaded);
 
 function domLoaded() {
-   // TODO: Complete the function
+   cInput.addEventListener("input", () => {
+      fInput.value = ""; 
+   })
+
+   fInput.addEventListener("input", () => {
+      cInput.value = ""; 
+   })
+
+   convertButton.addEventListener("click", (e) => {
+      e.preventDefault(); 
+
+      if (cInput.value !== "") {
+         var tempC = parseFloat(cInput.value);
+         if (isNaN(tempC) && cInput.value.search(" C") != -1) { 
+         var tempC2 = parseFloat(tempC);
+         if (isNaN(tempC2)) {
+            errorMessage.innerText = cInput.value + " is not a number";
+            errorMessage.style.color = "red";
+            return; 
+         }
+         }
+          
+         var arr = [];
+         if (cInput.value.search(" ") != -1) {
+         arr.split(" ");
+         var tempF = convertCtoF(arr[0]);
+         fInput.value = tempF; 
+         }
+         else {
+         var tempF = convertCtoF(tempC);
+         fInput.value = tempF; 
+         }
+         
+         if (tempF > 50) { 
+         weatherImage.src = "warm.png"; 
+         } else if (tempF >= 32 && tempF <= 50) { 
+            weatherImage.src = "cool.png";
+         } else { 
+            weatherImage.src = "cold.png";
+         }
+      }
+      
+      else if (fInput.value !== "" ) {
+         var tempF = parseFloat(fInput.value);
+         if (isNaN(tempF)) { 
+            tempF = parseFloat(fInput.value);
+            if (isNaN(tempF)) {
+               errorMessage.innerText = fInput.value + " is not a number";
+               errorMessage.style.color = "red";
+               return; 
+            }
+          }
+         var tempC = convertFtoC(tempF);
+         cInput.value = tempC; 
+
+         if (tempF > 50) { 
+         weatherImage.src = "warm.png"; 
+         } else if (tempF >= 32 && tempF <= 50) {
+         weatherImage.src = "cool.png";
+         } else {
+         weatherImage.src = "cold.png";
+         }
+      }
+   })
 }
 
 function convertCtoF(degreesCelsius) {
-   // TODO: Complete the function
+   return (degreesCelsius * 9/5 + 32);
 }
 
 function convertFtoC(degreesFahrenheit) {
-   // TODO: Complete the function
+   return ((degreesFahrenheit - 32) * 5/9);
 }
