4abacf35dbf5c1e1138bfd4990276c485977ffed
minshwemaunghtet <minshwemaunghtet@gmail.com>
2023-02-28 15:17:21 -0600

    convert.js

--- section 7/LAB_ Temperature converter/convert.js
+++ section 7/LAB_ Temperature converter/convert.js
@@ -14,16 +14,15 @@ function domLoaded() {
 
       if (cInput.value !== "") {
          var tempC = parseFloat(cInput.value);
-         if (isNaN(tempC) && cInput.value.search(" C") != -1) { 
-            var tempC2 = parseFloat(tempC);
+         if ((isNaN(tempC) && cInput.value.search(" C") != -1)||isNaN(tempC)) { 
+            var tempC2 = parseFloat(cInput.value);
+            errorMessage.innerHTML='';
          if (isNaN(tempC2)) {
-            errorMessage.innerText = cInput.value + " is not a number";
+            errorMessage.innerHTML  = `${cInput.value} is not a number`;
             errorMessage.style.color = "red";
-            return; 
-         }
-         }
-          
+            return  ;             }          }          
          var arr = [];
+         
          if (cInput.value.search(" ") != -1) {
             arr.split(" ");
             var tempF = convertCtoF(arr[0]);
@@ -32,46 +31,45 @@ function domLoaded() {
 
          else {
             var tempF = convertCtoF(tempC);
-            fInput.value = tempF; 
-         }
-         
+            fInput.value = tempF;           }        
          if (tempF > 50) { 
             weatherImage.src = "warm.png"; 
          } else if (tempF >= 32 && tempF <= 50) { 
             weatherImage.src = "cool.png";
          } else { 
             weatherImage.src = "cold.png";
-         }
+         }      
       }
       
       else if (fInput.value !== "" ) {
          var tempF = parseFloat(fInput.value);
          if (isNaN(tempF)) { 
             tempF = parseFloat(fInput.value);
-            if (isNaN(tempF)) {
-               errorMessage.innerText = fInput.value + " is not a number";
-               errorMessage.style.color = "red";
-               return; 
-            }
-          }
+         if (isNaN(tempF)) {
+            errorMessage.innerHTML  = `${fInput.value} is not a number`;
+            errorMessage.style.color = "red";
+            return ; 
+         }          
+      }
          var tempC = convertFtoC(tempF);
          cInput.value = tempC; 
 
          if (tempF > 50) { 
             weatherImage.src = "warm.png"; 
-         } else if (tempF >= 32 && tempF <= 50) {
+         } else if (tempF >= 32 && tempF <= 50) { 
             weatherImage.src = "cool.png";
-         } else {
+         } else { 
             weatherImage.src = "cold.png";
-         }
-      }
+         }      
+      }   
    })
 }
 
 function convertCtoF(degreesCelsius) {
-   return (degreesCelsius * 9/5 + 32);
-}
+   errorMessage.innerHTML=''
+   return (degreesCelsius * 9/5 + 32);}
 
 function convertFtoC(degreesFahrenheit) {
+   errorMessage.innerHTML=''
    return ((degreesFahrenheit - 32) * 5/9);
 }
